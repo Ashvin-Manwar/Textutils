8ca08b49098a6d2b2a14461712595d005e9315d1
Ashvin Manwar <manwar.ashvin@gmail.com>
2023-09-06 15:16:41 +0530

    Uploading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import React from "react";
 import "./App.css";
-import About from "./Components/About";
+// import About from "./Components/About";
 import Navbar from "./Components/Navbar";
 import { TextForm } from "./Components/TextForm";
 import Alert from "./Components/Alert";
 
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -36,27 +36,25 @@ function App() {
   };
   return (
     <>
-      <Router>
-        {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
-        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-        <Alert alert={alert} />
-        <div className="container my-3">
-          <Routes>
-            <Route exact path="/about" element={<About />}></Route>
-            <Route
-              exact
-              path="/"
-              element={
-                <TextForm
-                  heading="Enter text to analyze"
-                  mode={mode}
-                  showAlert={showAlert}
-                />
-              }
-            ></Route>
-          </Routes>
-        </div>
-      </Router>
+      {/* <Router> */}
+      {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        {/* <Routes> */}
+        {/* <Route exact path="/about" element={<About />}></Route> */}
+        {/* <Route */}
+        {/* exact  path="/"  element={ */}
+        <TextForm
+          heading="Enter text to analyze"
+          mode={mode}
+          showAlert={showAlert}
+        />
+        {/* } */}
+        {/* ></Route> */}
+        {/* </Routes> */}
+      </div>
+      {/* </Router> */}
     </>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
@@ -9,10 +9,11 @@ export default function Navbar(props) {
         className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
       >
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            {/* TextUtils */}
+          {/* <Link className="navbar-brand" to="/">TextUtils{props.title}</Link> */}
+          <a className="navbar-brand" href="#">
+            {" "}
             {props.title}
-          </Link>
+          </a>
           <button
             className="navbar-toggler"
             type="button"
@@ -27,16 +28,15 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                {/* <Link className="nav-link active" aria-current="page" to="/">  Home     </Link> */}
+                <a className="nav-link active" aria-current="page" href="#">
                   Home
-                </Link>
+                </a>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">
-                  {/* About */}
-                  {props.aboutText}
-                </Link>
-              </li>
+              {/* <li className="nav-item"> */}
+              {/* <Link className="nav-link" to="/about"> About{props.aboutText}  </Link> */}
+              {/* <a className="nav-link" href="#">                  {props.aboutText}                </a> */}
+              {/* </li> */}
             </ul>
             {/* <form className="d-flex" role="search">
               <input
